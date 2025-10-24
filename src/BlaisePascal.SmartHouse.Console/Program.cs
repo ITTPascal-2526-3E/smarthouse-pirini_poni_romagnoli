f88872bd16e241414f264c36724e290db064621e
“name” <samuele.romagnoli@gmail.com>
2025-10-24 10:34:14 +0200

    setup progetti

--- src/BlaisePascal.SmartHouse.Console/Program.cs
+++ src/BlaisePascal.SmartHouse.Console/Program.cs
@@ -1,10 +1,7 @@
-﻿namespace BlaisePascal.SmartHouse.Console
+﻿internal class Program
 {
-    internal class Program
+    static void Main(string[] args)
     {
-        static void Main(string[] args)
-        {
-            Console.WriteLine("Hello, World!");
-        }
+        Console.WriteLine("Hello, World!");
     }
 }
