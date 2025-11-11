d79d138dcab0d16947b46109e3d4fbe141ef829c
LorenzoPoni <lorenzo.poni09@gmail.com>
2025-11-11 13:30:27 +0100

    Added CCTV class and developed it. No other changes in other classes.

---
+++ src/BlaisePascal.SmartHouse.Domain/CCTV.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlTypes;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.SmartHouse.Domain
+{
+    internal class CCTV
+    {
+        private Guid Id { get; }
+        // CCTV attributes
+        private string Model { get; set; }
+        private string Brand { get; set; }
+        private string Resolution { get; set; }
+        public bool IsRecording { get; private set; }
+        private int zoomLevel { get; set; }
+        private int TelephotoLevel { get; set; }
+        private int WideAngleLevel { get; set; }
+        public bool IsNightVisionOn { get; private set; }
+        private string name { get; set; }
+
+        // Constructor
+        public CCTV(string model, string brand, string resolution, int cameraTelephotoLevel, int cameraWideAngleLevel, string cameraName)
+        {
+            Model = model;
+            Brand = brand;
+            Resolution = resolution;
+            IsRecording = false;
+            TelephotoLevel = cameraTelephotoLevel;
+            WideAngleLevel = cameraWideAngleLevel;
+            name = cameraName;
+            Id = Guid.NewGuid();
+        }
+        // Start recording
+        public void StartRecording()
+        {
+            zoomLevel = WideAngleLevel;
+            if (IsRecording == true)
+            {
+                return;
+            }
+            IsRecording = true;
+        }
+        // Stop recording
+        public void StopRecording()
+        {
+            if (IsRecording == false)
+            {
+                return;
+            }
+            IsRecording = false;
+            zoomLevel = 0;
+        }
+        public void zoom(int zoomLevelWanted)
+        {
+            if (IsRecording == false)
+            {
+                return;
+            }
+            else if (IsRecording == true)
+            {
+                if (zoomLevelWanted <= TelephotoLevel && zoomLevelWanted >= WideAngleLevel)
+                {
+                    zoomLevel = zoomLevelWanted;
+                }
+            }
+        }
+        public void ToggleNightVision()
+        {
+            if (IsNightVisionOn == false)
+            {
+                IsNightVisionOn = true;
+            }
+            else
+            {
+                IsNightVisionOn = false;
+            }
+        }
+
+    }
+}
