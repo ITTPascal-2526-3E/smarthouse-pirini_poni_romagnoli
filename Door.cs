f017bc13c6cd54dee83b2eb963bd363851d2e9ec
francescocorbarastud-ai <francesco.corbara.stud@ispascalcomandini.it>
2025-11-11 13:34:58 +0100

    added first version of class Door

---
+++ Door.cs
@@ -0,0 +1,40 @@
+﻿using System;
+public class Door()
+{
+    private Guid Id { get; }
+    public string Name { get; set; } //name that the user can assign to the device
+    public bool IsOpen { get; set; } //True if the door is open
+    public bool IsLock { get; set; } //True if the door is lock
+
+    public Door(string name)
+    {
+        Name = name;
+        IsOpen = false;
+        IsLock = true; 
+    }
+
+    public void OpenDoor()
+    {
+        IsOpen = true;
+    }
+
+    public void CloseDoor()
+    {
+        IsOpen = false;
+    }
+
+    public void LockDoor()
+    {
+        IsLock = true;
+    }
+
+    public void OpenDoorWithKey()
+    {
+        IsLock = false;
+    }
+}
+
+
+
+
+
