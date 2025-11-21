d76ff80acd1be0917904ba7e9a0e41dab4b1d5f3
SAMUELE.ROMAGNOLI.3e <SAMUELE.ROMAGNOLI.3e@PC4723>
2025-11-21 09:40:00 +0100

    coffee machine now in the right position

--- test/Blaisepascal.Smarthouse.Domain.unitTests/LampTest.cs
+++ test/Blaisepascal.Smarthouse.Domain.unitTests/LampTest.cs
@@ -6,8 +6,30 @@ using System.Threading.Tasks;
 
 namespace Blaisepascal.Smarthouse.Domain.unitTests
 {
-    internal class LampTest
+    public class LampTest
     {
-        
+        // Dati di test fissi e semplici
+        private const int Power = 10;
+        private const Lamp.ColorOption Color = Lamp.ColorOption.WarmWhite;
+        private const string Model = "XU-200";
+        private const string Brand = "SmartTech";
+        private const string EnergyClass = "A++";
+        private const string Name = "Lampada da scrivania";
+
+        private Lamp CreateDefaultLamp()
+        {
+            return new Lamp(Power, Color, Model, Brand, EnergyClass, Name);
+        }
+
+        [Fact]
+        public void Constructor_InitialStateIsOffAndZeroLuminosity()
+        {
+            // Arrange
+            var lamp = CreateDefaultLamp();
+
+            // Assert
+            Assert.False(lamp.IsOn);
+            Assert.Equal(0, lamp.LuminosityPercentage);
+        }
     }
 }
