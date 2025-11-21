34fb15e727b7603c1c5308294830e8ce78cf9ae0
LorenzoPoni <lorenzo.poni09@gmail.com>
2025-11-21 09:29:24 +0100

    Added the rest of necessary test classes.

---
+++ test/Blaisepascal.Smarthouse.Domain.unitTests/CCTVTest.cs
@@ -0,0 +1,8 @@
+namespace Blaisepascal.Smarthouse.Domain.unitTests;
+public class CCTVTest
+{
+    public void TestMethod1()
+    {
+
+    }
+}
---
+++ test/Blaisepascal.Smarthouse.Domain.unitTests/ThermostatTest.cs
@@ -0,0 +1,7 @@
+namespace Blaisepascal.Smarthouse.Domain.unitTests;
+public class ThermostatTest
+{
+    public void TestMethod1()
+    {
+    }
+}
