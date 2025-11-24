af938b79641763dad65f07872bfe4ab9d053fd9a
pirins2020 <pirini.alberto.stud@ispascalcomandini.it>
2025-11-24 10:27:41 +0100

    Added second version of CofeeMachine

---
+++ Office.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections;
+
+public class Ufficio
+{
+	public Ufficio()
+	{
+
+
+
+		public void Generator()
+		{
+			List<User> Users = new List<User>() { User us1, User us2, User us3};
+
+		}
+	}
+}
--- src/BlaisePascal.SmartHouse.Domain/CofeeMachine.cs
+++ src/BlaisePascal.SmartHouse.Domain/CofeeMachine.cs
@@ -2,5 +2,36 @@
 
 public class CofeeMachine
 {
-	
+    private Guid Id { get; } // Unique identifier for the cofee machine
+    public string Name { get; set; } // Name assigned by the user
+    public string Brand { get; set; } // The brand of the air conditioner
+    public string Model { get; set; } // The model of the air conditioner
+    public enum EnergyClass { A_plus_plus_plus, A_plus_plus, A_plus, A, B, C, D } // Energy efficiency standard classes
+    public EnergyClass EnergyEfficency; // The energy efficiency class of the air conditioner
+    public bool IsOn { get; set; } // True if the cofee machine is on
+    public DateTime? ScheduledOn { get; private set; } // Scheduled time to turn ON
+    public DateTime? ScheduledOff { get; private set; } // Scheduled time to turn OFF       
+
+    public CofeeMachine(Guid id, string name, string brand, string model, EnergyClass energyEfficency)
+    {
+        Id = id;
+        Name = name;
+        Brand = brand;
+        Model = model;
+        EnergyEfficency = energyEfficency;
+        IsOn = false;
+    }
+
+    public void turnOn()
+    { 
+        IsOn = true;
+    }
+
+    public void turnOff()
+    { 
+        IsOn = false;
+    }
+
+    public void changeName(string name)
+    { }
 }
