ba33392de847c7b4bc1f488d82d92982dede6322
lorenzo.poni09@gmail.com <lorenzo.poni09@gmail.com>
2026-02-19 21:58:43 +0100

    Added all the folders in Application Layer, including the Commands and Queries ones.

---
+++ src/BlaisePascal.Smarthouse.Application/Heating/Repositories/Queries/GetHeatPumpByIDQuery.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BlaisePascal.Smarthouse.Application.Heating.Repositories.Queries
+{
+    internal class GetHeatPumpByIDQuery
+    {
+    }
+}
