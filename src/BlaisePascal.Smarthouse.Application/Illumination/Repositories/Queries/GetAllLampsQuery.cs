06c5ddbb533748e455b664337220c682929907e5
lorenzo.poni09@gmail.com <lorenzo.poni09@gmail.com>
2026-02-23 20:11:53 +0100

    (Added Queries, forgot to in the first commit)

--- src/BlaisePascal.Smarthouse.Application/Illumination/Repositories/Queries/GetAllLampsQuery.cs
+++ src/BlaisePascal.Smarthouse.Application/Illumination/Repositories/Queries/GetAllLampsQuery.cs
@@ -3,10 +3,22 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using BlaisePascal.SmartHouse.Domain.Illumination.Repositories;
+using BlaisePascal.SmartHouse.Domain.Illumination.LampTypes;
+using BlaisePascal.SmartHouse.Domain.Illumination.LampOptions;
 
 namespace BlaisePascal.SmartHouse.Application.Illumination.Repositories.Queries
 {
-    internal class GetAllLampsQuery
+    public class GetAllLampsQuery
     {
+        private readonly ILampRepository _lampRepository;
+        public GetAllLampsQuery(ILampRepository lampRepository)
+        {
+            _lampRepository = lampRepository;
+        }
+        public List<Lamp> Execute()
+        {
+            return _lampRepository.GetAll();
+        }
     }
 }
